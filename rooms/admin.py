47013d04b413a252c6f0c8a885359738c3da6a86
Nate_River <crimson747@Nate-Riverui-MacBookPro.local>
2020-02-22 16:48:17 +0900

    Room Admin Panel part Two

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,6 +15,23 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    fieldsets = (
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")},),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Detailse", {"fields": ("host",)}),
+    )
+
     list_display = [
         "name",
         "country",
@@ -31,11 +48,18 @@ class RoomAdmin(admin.ModelAdmin):
 
     list_filter = [
         "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
         "city",
         "country",
     ]
 
     search_fields = ["^city", "^host__username"]
+
+    filter_horizontal = ["amenities", "facilities", "house_rules"]
 
 
 @admin.register(models.Photo)
